37a05fda7e506140b850fc511043d2ec907853b3
Rafael Felix <felix.rafael@gmail.com>
2011-04-19 12:57:46 -0300

    App criada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Takshub::Application.config.secret_token = '1039643e3deda893b9cc229c506b00b386f0455066bf66621b0d24b7baf5ab151511be31b84463367cd6b82b051f0d680ff3ae17d635853826263e6e907b5d02'
